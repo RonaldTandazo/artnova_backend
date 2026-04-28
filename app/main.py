4399f0d02f1177986d9292cc054c69897df8928d
RonaldTandazo <edu09085@hotmail.com>
2026-04-28 03:16:04 +0000

    fixing cors

--- app/main.py
+++ app/main.py
@@ -24,10 +24,13 @@ async def shutdown_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://artnova-frontend.vercel.app"],
+    allow_origins=[
+        "https://artnova-frontend.vercel.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 print("URL_FRONTEND:", repr(URL_FRONTEND))
