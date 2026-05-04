0693d96bfaed82a0b71e16790b9eacbd42c8e6ad
RonaldTandazo <edu09085@hotmail.com>
2026-05-04 02:35:26 +0000

    allowing google.com

--- app/main.py
+++ app/main.py
@@ -25,10 +25,10 @@ async def shutdown_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[URL_FRONTEND],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 mimetypes.add_type('model/gltf+json', '.gltf')
